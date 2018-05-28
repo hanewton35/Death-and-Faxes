23e3f16597caa0db7527a03d84189c05e0a1071d
Henry <hanewton35@gmail.com>
2018-05-28 01:04:23 -0400

    l

--- FullGame/EmpOrGla.java
+++
@@ -1,7 +0,0 @@
-public class EmpOrGla extends Obj
-{
-    public EmpOrGla(Board board, int row, int col)
-    {
-        super(board, row, col);
-    }
-}
--- FullGame/EmpOrGla2.java
+++
@@ -1,7 +0,0 @@
-public class EmpOrGla2 extends Obj2
-{
-    public EmpOrGla2(Board2 board, int row, int col)
-    {
-        super(board, row, col);
-    }
-}
